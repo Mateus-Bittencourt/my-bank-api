541ab370d2e22c53ac4fdfc737102cdf7ee98319
Mateus Bittencourt <mateusmbittencourt@gmail.com>
2023-01-22 18:22:37 -0300

    implement get and delete routes

--- routes/accounts.js
+++ routes/accounts.js
@@ -3,27 +3,71 @@ import { promises as fs } from "fs";
 
 const { readFile, writeFile } = fs;
 
+global.fileName = "accounts.json";
+
 const router = express.Router();
 
 router.post("/", async (req, res) => {
   try {
-
     let account = req.body;
-    const data = JSON.parse(await readFile("accounts.json"));
+    const data = JSON.parse(await readFile(global.fileName));
 
     account = { id: data.nextId, ...account };
-    account.id = data.nextId
+    account.id = data.nextId;
     data.nextId++;
     data.accounts.push(account);
 
-    await writeFile("accounts.json", JSON.stringify(data));
+    await writeFile(global.fileName, JSON.stringify(data, null, 2));
 
     res.statusCode = 201;
     res.send(account);
   } catch (error) {
     res.status(400).send({ error: error.message });
   }
+});
 
+router.get("/", async (req, res) => {
+  try {
+    const data = JSON.parse(await readFile(global.fileName));
+    delete data.nextId;
+    res.send(data);
+  } catch (error) {
+    res.status(400).send({ error: error.message });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const data = JSON.parse(await readFile(global.fileName));
+    const account = data.accounts.find(
+      (account) => account.id === parseInt(req.params.id)
+    );
+    if (!account) {
+      res.statusCode = 404;
+      res.send({ message: "Account not found" });
+    }
+    res.send(account);
+  } catch (error) {
+    res.status(400).send({ error: error.message });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const data = JSON.parse(await readFile(global.fileName));
+
+    data.accounts = data.accounts = data.accounts.filter(
+      (account) => account.id !== parseInt(req.params.id)
+    );
+
+    await writeFile(global.fileName, JSON.stringify(data, null, 2));
+
+    res.statusCode = 204;
+    res.end();
+
+  } catch (error) {
+    res.status(400).send({ error: error.message });
+  }
 });
 
 export default router;
